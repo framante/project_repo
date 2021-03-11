72d7ea4ea5e26321797887993083898d1adb7a2a
Francesco Mantegazza <framante95@gmail.com>
2021-03-11 17:06:05 +0000

    added main2.cpp

---
+++ main2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+  int a = 10;
+  std::cout << "input integer number" << std::endl;
+  std::cin >> a;
+  std::cout << "the input number is " << a << std::endl;
+  return 0;
+}
