439cb89011914fd6c8244ebfa910e43a84e9f10a
Francesco Mantegazza <framante95@gmail.com>
2021-03-11 14:38:41 +0000

    added a printing line

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char ** argv)
 {
   myclass mm(5, 7.0);
   std::cout << "this is a modification" << std::endl;
+  std::cout << "I also print the content of myclass" << std::endl;
   std::cout << "my class contains " << mm.get_i() << " , " <<
     mm.get_j() << std::endl;
 
