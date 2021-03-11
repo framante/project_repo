4cbed7d044633823d92b415dc1d13ac7382313f4
Francesco Mantegazza <framante95@gmail.com>
2021-03-11 14:22:37 +0000

    introduction of myclass in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <cmath>
+#include "myclass.hpp"
 
 int main(int argc, char ** argv)
 {
-  
+  myclass mm(5, 7.0);
   std::cout << "this is a modification" << std::endl;
+  std::cout << "my class contains " << mm.get_i() << " , " <<
+    mm.get_j() << std::endl;
+
   return 0;
 }
