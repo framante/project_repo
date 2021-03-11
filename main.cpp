a0187e7ec724c1f0e469ae977617b4d03d23a2af
Francesco Mantegazza <framante95@gmail.com>
2021-03-11 11:11:55 +0000

    two things done:no print, include

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <cmath>
 
 int main(int argc, char ** argv)
 {
-  std::cout << "hello world!\n" << std::endl;
+  
   std::cout << "this is a modification" << std::endl;
   return 0;
 }
