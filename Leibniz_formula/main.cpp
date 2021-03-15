f1296a92b501c575143560ac7664e32137eb33a9
Francesco Mantegazza <framante95@gmail.com>
2021-03-15 14:53:48 +0000

    added Richardson extrapolation to accelerate convergence

--- Leibniz_formula/main.cpp
+++ Leibniz_formula/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <vector>
 
 typedef double real;
 
 real Liebniz(unsigned);
 real Liebniz_2(unsigned);
+real Richardson_extrapolation(unsigned, real);
+constexpr inline real  int_pow(real base, real exponent);
+
 
 int main(int argc, char ** argv)
 {
@@ -12,11 +16,14 @@ int main(int argc, char ** argv)
   std::cin >> iterations;
   real pi1 = Liebniz(iterations);
   real pi2 = Liebniz_2(iterations);
+  real pi3 = Richardson_extrapolation(iterations, 0.2);
 
   std::cout << "for " << iterations << " iterations, pi is: " <<
     pi1 << std::endl;
   std::cout << "for " << iterations << " iterations, pi is: " <<
     pi2 << std::endl;
+  std::cout << "for " << iterations << " iterations, pi is: " <<
+    pi3 << std::endl;
 
 
   return 0; 
@@ -46,3 +53,34 @@ real Liebniz_2(unsigned iterations)
   return 4.*pi;
 }
 
+
+real Richardson_extrapolation(unsigned iterations, real t)
+{
+  std::vector<real>v1(iterations);
+  std::vector<real>v2(iterations);
+
+  unsigned h = 1;
+  v1[0] = Liebniz_2(h);
+
+  for(unsigned i = 1; i < iterations; ++i)
+    {
+      h *= t;
+      v2[0] = Liebniz_2(h);
+
+      for(unsigned j = 0; j < i; j++)
+	{
+	  const real c = int_pow(t, j+1);
+	  v2[j+1] = (c * v2[j] - v1[j]) / (c - 1);
+	}
+      std::swap(v1, v2);
+    }
+  return v1.back();
+}
+
+constexpr inline real  int_pow(real base, real exponent)
+{
+ real result = 1.;
+ for(unsigned i = 0; i < exponent; i++)
+  result *= base;
+ return result;
+}
