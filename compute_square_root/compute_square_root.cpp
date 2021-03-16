df7c07ae794affd1bf3186d1c2df88aa871920d9
Francesco Mantegazza <framante95@gmail.com>
2021-03-16 11:05:27 +0000

    generalization to negative input

--- compute_square_root/compute_square_root.cpp
+++ compute_square_root/compute_square_root.cpp
@@ -1,31 +1,41 @@
 #include <iostream>
 #include <algorithm>
+#include <complex>
 
 typedef float real;
 
-real compute_square_root(real);
+std::complex<real> compute_square_root(real);
 
 int main(int argc, char ** argv)
 {
   real x = 0.;
   std::cout << "insert a number: ";
   std::cin >> x;
-  real y = compute_square_root(x);
+  std::complex<real> y = compute_square_root(x);
   std::cout << "the square root of " << x << " is: " << y << std::endl;
   return 0;
 }
 
-real compute_square_root(real x)
+std::complex<real> compute_square_root(real x)
 {
   real tol = 1e-7;
   if(x == 0)
     return x;
-  real y0 = x;
-  real y1 = 0.5 * std::max(x, 1.f);
-  while(std::abs(y1 - y0) > tol)
+  else if(x > 0)
     {
-      y0 = y1;
-      y1 = 0.5 * (y0 + x/y0);
+      real y0 = x;
+      real y1 = 0.5 * std::max(x, 1.f);
+      while(std::abs(y1 - y0) > tol)
+	{
+	  y0 = y1;
+	  y1 = 0.5 * (y0 + x/y0);
+	}
+      return std::complex<real>(y1, 0.);
     }
-  return y1;    
+  else
+    {
+      std::complex<real> sqrt = compute_square_root(-x);
+      return std::complex<real>(0., sqrt.real());
+    }
 }
+
