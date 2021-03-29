fb6a68c2b9f7e4700a2ada54107861fd3b162474
Francesco Mantegazza <framante95@gmail.com>
2021-03-29 07:58:48 +0000

    solved problem related to insertion with comments too

--- insertion/main.cpp
+++ insertion/main.cpp
@@ -10,7 +10,8 @@ int main(int argc, char ** argv)
   std::vector<int> a;
   std::vector<int> b;
   std::set<int> c;
-  
+  std::vector<int> d (2, 0);
+		     
   // fill a and b with values
   a.insert(a.begin(), {1,2,4,7,3});
   b.insert(b.begin(), {5,3,2,10,9});
@@ -19,13 +20,22 @@ int main(int argc, char ** argv)
   // sort vectors
   std::sort(a.begin(),a.end());
   std::sort(b.begin(),b.end());
-  
-  std::set_intersection(a.begin(),a.end(),b.begin(),b.end(),
-			std::back_inserter(intersect));
-  // std::inserter(intersect.begin(), intersect)
-  std::set_intersection(a.begin(),a.end(),b.begin(),b.end(),
-			c.begin());
 
+  // insertion in an empty vector intersect. Here you need back_inserter
+  // since the vector is empty and you have to do push_back for every
+  // element
+  std::set_intersection(a.begin(),a.end(),b.begin(),b.end(),
+			std::back_inserter(intersect) );
+  
+  // insertion in an empty set. Here you need inserter since you have to
+  // insert eache element in a set, notice that the second argument is just
+  // a hint because the structure is managed based on the order relation
+  std::set_intersection(a.begin(),a.end(),b.begin(),b.end(),
+			std::inserter(c, c.begin()) );
+
+  // insertion in a resized vector. Here you don't need back_inserter since
+  // the vector has already been resized conveniently
+  std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), d.begin());
   
   for(auto & elem: a)
     std::cout << elem << " ";
@@ -42,6 +52,10 @@ int main(int argc, char ** argv)
   for(auto & elem: c)
     std::cout << elem << " ";
   std::cout << std::endl;
-  
+
+  for(auto & elem: d)
+    std::cout << elem << " ";
+  std::cout << std::endl;
+
   return 0;
 }
